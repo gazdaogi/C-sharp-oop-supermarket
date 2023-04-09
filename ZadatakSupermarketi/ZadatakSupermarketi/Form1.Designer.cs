0d50313881bcd250974e4ff942713838e05a80d8
Ognjen Jovanović <130289717+gazdaogi@users.noreply.github.com>
2023-04-09 16:10:15 +0200

    Add files via upload

---
+++ ZadatakSupermarketi/ZadatakSupermarketi/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ZadatakSupermarketi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Voce = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Supermarket = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cena = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Voce,
+            this.Supermarket,
+            this.Cena});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(460, 381);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Voce
+            // 
+            this.Voce.HeaderText = "Voce";
+            this.Voce.Name = "Voce";
+            this.Voce.ReadOnly = true;
+            // 
+            // Supermarket
+            // 
+            this.Supermarket.HeaderText = "Supermarket";
+            this.Supermarket.Name = "Supermarket";
+            this.Supermarket.ReadOnly = true;
+            // 
+            // Cena
+            // 
+            this.Cena.HeaderText = "Cena";
+            this.Cena.Name = "Cena";
+            this.Cena.ReadOnly = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(507, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Otvori datoteku";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(699, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Datoteka nije otvorena";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(507, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 47);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Procescne vrednosti voca";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(507, 173);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 46);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Najpovoljniji supermarket";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 484);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Voce;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Supermarket;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cena;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ZadatakSupermarketi/ZadatakSupermarketi/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security;
+
+namespace ZadatakSupermarketi
+{
+    public partial class Form1 : Form
+    {
+        //private String file;
+        private Dictionary<String, int> gomex = new Dictionary<String,int>();
+        private Dictionary<String, int> idea = new Dictionary<String, int>();
+        private Dictionary<String, int> maxi = new Dictionary<String, int>();
+        private Dictionary<String, int> perSu = new Dictionary<String, int>();
+        private Dictionary<String, int> univer = new Dictionary<String, int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += new EventHandler(SelectButton_Click);
+        }
+
+        //open file dialog
+        private void SelectButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var sr = new StreamReader(openFileDialog1.FileName);
+                    String file = sr.ReadToEnd();
+                    string[] stringSeparators = new string[] { "\r\n" };
+                    string[] lines = file.Split(stringSeparators, StringSplitOptions.None);
+
+                    foreach (String line in lines)
+                    {
+                        if (line.Equals(""))
+                        {
+                            break;
+                        }
+
+                        String[] lineParts = line.Split(' ');
+                        String market = lineParts[0];
+                        String fruit = lineParts[1];
+                        int price = Convert.ToInt32(lineParts[2]);
+                        
+                        if(market.Equals("Gomex"))
+                        {
+                            this.gomex.Add(fruit,price);
+                        }
+                        else if (market.Equals("Idea"))
+                        {
+                            this.idea.Add(fruit,price);
+                        }
+                        else if (market.Equals("Maxi"))
+                        {
+                            this.maxi.Add(fruit,price);
+                        }
+                        else if (market.Equals("UniverExport"))
+                        {
+                            this.univer.Add(fruit,price);
+                        }
+                        else //PerSu
+                        {
+                            this.perSu.Add(fruit,price);
+                        }
+
+                        this.dataGridView1.Rows.Add(market,fruit,price);
+                    }
+
+                    label1.Text = "Datoteka " + openFileDialog1.FileName;
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show("Security error.\n\nError message: {ex.Message}\n\n" +
+                    "Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+
+        //najpovoljniji supermarket
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int fruitsSumGomex = 0;
+            int fruitsSumPerSu = 0;
+            int fruitsSumIdea = 0;
+            int fruitsSumMaxi = 0;
+            int fruitsSumUniver = 0;
+
+            foreach(int price in gomex.Values)
+            {
+                fruitsSumGomex += price;
+            }
+            foreach (int price in idea.Values)
+            {
+                fruitsSumIdea += price;
+            }
+            foreach (int price in perSu.Values)
+            {
+                fruitsSumPerSu += price;
+            }
+            foreach (int price in univer.Values)
+            {
+                fruitsSumUniver += price;
+            }
+            foreach (int price in maxi.Values)
+            {
+                fruitsSumMaxi += price;
+            }
+
+            int minValue = fruitsSumGomex;
+            String marketWithMinValue = "Gomex";
+
+            if (fruitsSumIdea < minValue)
+            {
+                minValue = fruitsSumIdea;
+                marketWithMinValue = "Idea";
+            }
+            if (fruitsSumMaxi < minValue)
+            {
+                minValue = fruitsSumMaxi;
+                marketWithMinValue = "Maxi";
+            }
+            if (fruitsSumPerSu < minValue)
+            {
+                minValue = fruitsSumPerSu;
+                marketWithMinValue = "PerSu";
+            }
+            if (fruitsSumUniver < minValue)
+            {
+                minValue = fruitsSumUniver;
+                marketWithMinValue = "Univer";
+            }
+
+            MessageBox.Show("Najpovoljniji supermarket je " + marketWithMinValue
+                + ", sa ukupnom cenom proizvoda " + minValue);
+
+
+        }
+
+        //prosecne vrednosti voca
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double avgBananas = this.getAveragePriceOfFruit("Banane");
+            double avgLemons = this.getAveragePriceOfFruit("Limun");
+            double avgApples = this.getAveragePriceOfFruit("Jabuke");
+            double avgOranges = this.getAveragePriceOfFruit("Pomorandze");
+            double avgPineapple = this.getAveragePriceOfFruit("Ananas");
+
+            String msg = "Prosecna cena banana: " + avgBananas;
+            msg += Environment.NewLine;
+            msg += "Prosecna cena pomorandzi: " + avgOranges;
+            msg += Environment.NewLine;
+            msg += "Prosecna cena ananasa: " + avgPineapple;
+            msg += Environment.NewLine;
+            msg += "Prosecna cena jabuka: " + avgApples;
+            msg += Environment.NewLine;
+            msg += "Prosecna cena limuna: " + avgLemons;
+            msg += Environment.NewLine;
+
+            MessageBox.Show(msg);
+        }
+
+        private double getAveragePriceOfFruit(String fruitName)
+        {
+            int sumOfFruit = 0;
+            sumOfFruit += this.gomex[fruitName];
+            sumOfFruit += this.idea[fruitName];
+            sumOfFruit += this.maxi[fruitName];
+            sumOfFruit += this.univer[fruitName];
+            sumOfFruit += this.perSu[fruitName];
+            double avgFruit = (double)sumOfFruit / 5;
+
+            return avgFruit;
+        }
+
+
+
+    }
+}
